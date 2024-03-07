eecbae88484010df0d0e4d95e8a2db9ebd581ef7
Mohammed Khalid <mohammed.osman.a@gmail.com>
2024-03-07 07:21:24 +0000

    test: refactor tests to use test func

--- src/mars-rover.spec.ts
+++ src/mars-rover.spec.ts
@@ -20,12 +20,8 @@ describe.each([
   { input: { x: 0, y: 0, direction: 'N' }, commands: 'b', expected: { x: 0, y: -1, direction: 'N' } }
 ])('Mars Rover', ({ input, commands, expected }) => {
   test(`moves after receiving one command: ${input.x}, ${input.y}, ${commands}, facing ${input.direction}`, () => {
-    const startingPositionRover = new MarsRover(
-      Position.at(input.x, input.y).facing(input.direction)
-    );
-    const expectedPositionRover = new MarsRover(
-      Position.at(expected.x, expected.y).facing(expected.direction)
-    );
+    const startingPositionRover = initialiseRover(input.x, input.y, input.direction);
+    const expectedPositionRover = initialiseRover(expected.x, expected.y, expected.direction);
 
     startingPositionRover.move(commands);
 
