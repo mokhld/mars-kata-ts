af067d5d305f8866257d15722310679d73b08b2b
Mohammed Khalid <mohammed.osman.a@gmail.com>
2024-03-07 07:06:29 +0000

    chore: linting

--- src/mars-rover.spec.ts
+++ src/mars-rover.spec.ts
@@ -1,4 +1,4 @@
-import { MarsRover, Position } from './mars-rover'
+import { MarsRover, Position } from './mars-rover';
 
 describe.each([
   {
@@ -9,13 +9,12 @@ describe.each([
   test(`stays at the landing position: ${input.x}, ${input.y} facing ${input.direction}`, () => {
     const startingPositionRover = new MarsRover(
       Position.at(input.x, input.y).facing(input.direction)
-    )
-
+    );
     const expectedPositionRover = new MarsRover(
       Position.at(expected.x, expected.y).facing(expected.direction)
-    )
+    );
 
     // no movement
-    expect(startingPositionRover).toEqual(expectedPositionRover)
-  })
-})
+    expect(startingPositionRover).toEqual(expectedPositionRover);
+  });
+});
--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -1,14 +1,14 @@
 export class MarsRover {
-  private position: Position
+  private position: Position;
   constructor(position: Position) {
-    this.position = position
+    this.position = position;
   }
 }
 
 export class Direction {
-  private facing: string
+  private facing: string;
   constructor(facing: string) {
-    this.facing = facing
+    this.facing = facing;
   }
 }
 
@@ -16,19 +16,19 @@ export class Position {
   public static at(x: number, y: number) {
     return {
       facing(directionRaw: string) {
-        const direction = new Direction(directionRaw)
-        return new Position(x, y, direction)
+        const direction = new Direction(directionRaw);
+        return new Position(x, y, direction);
       },
-    }
+    };
   }
 
-  private x: number
-  private y: number
-  private direction: Direction
+  private x: number;
+  private y: number;
+  private direction: Direction;
 
   private constructor(x: number, y: number, direction: Direction) {
-    this.x = x
-    this.y = y
-    this.direction = direction
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
   }
 }
