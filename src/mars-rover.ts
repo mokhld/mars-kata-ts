5402cb127b70f4fca550c4acb0af0883ccdb0612
Mohammed Khalid <mohammed.osman.a@gmail.com>
2024-03-09 10:38:56 +0000

    chore: refactors: - use maps for other commands to others - remove if statemetns for map of commands - move movement config from Rover to Direction - convert movement map to a vector - removed unused methods - make directional pure

--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -1,77 +1,54 @@
-interface PositionOperations {
-  increaseY: () => void;
-  decreaseY: () => void;
-  increaseX: () => void;
-  decreaseX: () => void;
-}
-
-type DirectionType = 'N' | 'W' | 'S' | 'E';
-
-type MapType = {
-  [K in DirectionType]: {
-    left: DirectionType,
-    right: DirectionType,
-    forward: (t: PositionOperations) => void,
-    backward: (t: PositionOperations) => void
-  }
-};
-
 export class MarsRover {
   private static map = {
-    f: (position: { forward: () => void; }) => position.forward(),
-    b: (position: { backward: () => void; }) => position.backward(),
-    l: (position: { turnLeft: () => void; }) => position.turnLeft(),
-    r: (position: { turnRight: () => void; }) => position.turnRight(),
+    f: (position) => position.forward(),
+    b: (position) => position.backward(),
+    l: (position) => position.turnLeft(),
+    r: (position) => position.turnRight(),
   };
-
   private position: Position;
+
   constructor(position: Position) {
     this.position = position;
   }
 
   public move(commands: string) {
-    commands.toLowerCase().split('').forEach((command) => {
-      if (this.isValidCommand(command)) {
+    commands.split('').map((command) => {
+      try {
         MarsRover.map[command](this.position);
-      } else {
+      } catch (e) {
         throw Error(`Invalid command '${command}'`);
       }
     });
   }
-
-  private isValidCommand(command: string): command is keyof typeof MarsRover.map {
-    return command in MarsRover.map;
-  }
 }
 
-
 export class Direction {
-  public facing: DirectionType;
-  private static map: MapType = {
-    N: { left: 'W', right: 'E', forward: (t) => t.increaseY(), backward: (t) => t.decreaseY() },
-    W: { left: 'S', right: 'N', forward: (t) => t.decreaseX(), backward: (t) => t.increaseX() },
-    S: { left: 'E', right: 'W', forward: (t) => t.decreaseY(), backward: (t) => t.increaseY() },
-    E: { left: 'N', right: 'S', forward: (t) => t.increaseX(), backward: (t) => t.decreaseX() }
+  private static map = {
+    N: { left: 'W', right: 'E', forward: { x: 0, y: 1 }, backward: { x: 0, y: -1 } },
+    W: { left: 'S', right: 'N', forward: { x: -1, y: 0 }, backward: { x: 1, y: 0 } },
+    S: { left: 'E', right: 'W', forward: { x: 0, y: -1 }, backward: { x: 0, y: 1 } },
+    E: { left: 'N', right: 'S', forward: { x: 1, y: 0 }, backward: { x: -1, y: 0 } }
   };
+  public facing: string;
 
-  constructor(facing: DirectionType) {
+  constructor(facing: string) {
     this.facing = facing;
   }
 
   public left() {
-    this.facing = Direction.map[this.facing].left;
+    return new Direction(Direction.map[this.facing].left);
   }
 
   public right() {
-    this.facing = Direction.map[this.facing].right;
+    return new Direction(Direction.map[this.facing].right);
   }
 
-  public forward(x: PositionOperations) {
-    Direction.map[this.facing].forward(x);
+  public forward() {
+    return Direction.map[this.facing].forward;
   }
 
-  public backward(x: PositionOperations) {
-    Direction.map[this.facing].backward(x);
+  public backward() {
+    return Direction.map[this.facing].backward;
   }
 }
 
@@ -79,19 +56,15 @@ export class Position {
   public static at(x: number, y: number) {
     return {
       facing(directionRaw: string) {
-        if (!['N', 'W', 'S', 'E'].includes(directionRaw)) {
-          throw new Error(`Invalid direction '${directionRaw}'. Must be one of 'N', 'W', 'S', 'E'.`);
-        }
-
-        const direction = new Direction(directionRaw as DirectionType);
+        const direction = new Direction(directionRaw);
         return new Position(x, y, direction);
-      },
+      }
     };
   }
 
+  private direction: Direction;
   private x: number;
   private y: number;
-  private direction: Direction;
 
   private constructor(x: number, y: number, direction: Direction) {
     this.x = x;
@@ -99,35 +72,35 @@ export class Position {
     this.direction = direction;
   }
 
-  public increaseY() {
-    this.y++;
-  }
-
-  public increaseX() {
-    this.x++;
-  }
-
-  public decreaseY() {
-    this.y--;
-  }
-
-  public decreaseX() {
-    this.x--;
-  }
-
   public turnLeft() {
-    this.direction.left();
+    this.direction = this.direction.left();
   }
 
   public turnRight() {
-    this.direction.right();
+    this.direction = this.direction.right();
   }
 
   public forward() {
-    this.direction.forward(this);
+    this.sumVector(this.direction.forward());
   }
 
   public backward() {
-    this.direction.backward(this);
+    this.sumVector(this.direction.backward());
+  }
+
+  private sumVector(vector) {
+    this.x += vector.x;
+    this.y += vector.y;
+  }
+
+}
+
+export class World {
+  public static unlimited() {
+    return {};
+  }
+
+  public static wrapping(width: number, height: number) {
+    return {};
   }
 }
