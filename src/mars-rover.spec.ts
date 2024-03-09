d07f1bd61171e6cf8b0445d6033d97e93cb766fb
Mohammed Khalid <mohammed.osman.a@gmail.com>
2024-03-09 10:39:36 +0000

    test: apply world limits (plus refactor of tests)

--- src/mars-rover.spec.ts
+++ src/mars-rover.spec.ts
@@ -1,6 +1,6 @@
-import { Direction, MarsRover, Position } from './mars-rover';
+import { Direction, MarsRover, Position, World } from './mars-rover';
 
-function initialiseRover(x: number, y: number, directionRaw: string) {
+function initialiseRover(x, y, directionRaw, world: World = World.unlimited()) {
   return new MarsRover(Position.at(x, y).facing(directionRaw));
 }
 
@@ -11,7 +11,20 @@ describe('Mars Rover', () => {
 
     expect(() => {
       startingPositionRover.move('X');
-    }).toThrow("Invalid command 'x'");
+    }).toThrow("Invalid command 'X'");
+  });
+
+  describe.each([
+    { input: { x: 0, y: 0, direction: 'N' }, expected: { x: 0, y: 0, direction: 'N' } },
+  ])('stays at the landing position', ({ input, expected }) => {
+    test(`${input.x}, ${input.y} facing ${input.direction}`, () => {
+      const startingPositionRover = initialiseRover(input.x, input.y, input.direction);
+      const expectedPositionRover = initialiseRover(expected.x, expected.y, expected.direction);
+
+      // no movement
+
+      expect(startingPositionRover).toEqual(expectedPositionRover);
+    });
   });
 
   describe.each([
@@ -20,12 +33,11 @@ describe('Mars Rover', () => {
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'l', expected: { x: 0, y: 0, direction: 'W' } },
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'r', expected: { x: 0, y: 0, direction: 'E' } },
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'ff', expected: { x: 0, y: 2, direction: 'N' } },
-    { input: { x: 0, y: 0, direction: 'N' }, commands: 'FF', expected: { x: 0, y: 2, direction: 'N' } },
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'fb', expected: { x: 0, y: 0, direction: 'N' } },
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'lf', expected: { x: -1, y: 0, direction: 'W' } },
     { input: { x: 0, y: 0, direction: 'S' }, commands: 'f', expected: { x: 0, y: -1, direction: 'S' } },
-  ])('Mars Rover', ({ input, commands, expected }) => {
-    test(`moves after receiving commands '${commands}': ${input.x}, ${input.y}, ${commands}, facing ${input.direction}`, () => {
+  ])('moves after receiving commands', ({ input, commands, expected }) => {
+    test(`'${commands}': ${input.x}, ${input.y} facing ${input.direction}`, () => {
       const startingPositionRover = initialiseRover(input.x, input.y, input.direction);
       const expectedPositionRover = initialiseRover(expected.x, expected.y, expected.direction);
 
@@ -40,12 +52,13 @@ describe('Mars Rover', () => {
     { input: { x: 0, y: 0, direction: 'S' }, commands: 'f', expected: { x: 0, y: -1 } },
     { input: { x: 0, y: 0, direction: 'E' }, commands: 'f', expected: { x: 1, y: 0 } },
     { input: { x: 0, y: 0, direction: 'W' }, commands: 'f', expected: { x: -1, y: 0 } },
+
     { input: { x: 0, y: 0, direction: 'N' }, commands: 'b', expected: { x: 0, y: -1 } },
     { input: { x: 0, y: 0, direction: 'S' }, commands: 'b', expected: { x: 0, y: 1 } },
     { input: { x: 0, y: 0, direction: 'E' }, commands: 'b', expected: { x: -1, y: 0 } },
     { input: { x: 0, y: 0, direction: 'W' }, commands: 'b', expected: { x: 1, y: 0 } },
-  ])('Mars Rover', ({ input, commands, expected }) => {
-    test(`advancing movements never affect direction. Move '${commands}'. ${input.x}, ${input.y} facing ${input.direction}`, () => {
+  ])('advancing movements never affect direction', ({ input, commands, expected }) => {
+    test(`Move '${commands}'. ${input.x}, ${input.y} facing ${input.direction}`, () => {
       const startingPositionRover = initialiseRover(input.x, input.y, input.direction);
       const expectedPositionRover = initialiseRover(expected.x, expected.y, input.direction);
 
@@ -60,13 +73,13 @@ describe('Mars Rover', () => {
     { input: 'W', expected: 'S' },
     { input: 'S', expected: 'E' },
     { input: 'E', expected: 'N' },
-  ])('Position', ({ input, expected }) => {
-    test(`turn left (${input})`, () => {
+  ])('Turn left', ({ input, expected }) => {
+    test(`${input}`, () => {
       const direction = new Direction(input);
 
-      direction.left();
+      const actual = direction.left();
 
-      expect(direction).toEqual(new Direction(expected));
+      expect(actual).toEqual(new Direction(expected));
     });
   });
 
@@ -75,13 +88,27 @@ describe('Mars Rover', () => {
     { input: 'W', expected: 'N' },
     { input: 'S', expected: 'W' },
     { input: 'E', expected: 'S' },
-  ])('Position', ({ input, expected }) => {
-    test(`turn right (${input})`, () => {
+  ])('Turn Right', ({ input, expected }) => {
+    test(`${input}`, () => {
       const direction = new Direction(input);
 
-      direction.right();
+      const actual = direction.right();
 
-      expect(direction).toEqual(new Direction(expected));
+      expect(actual).toEqual(new Direction(expected));
+    });
+  });
+
+  describe.each([
+    { side: 'left', input: { x: 0, y: 0, direction: 'W' }, commands: 'f', expected: { x: 15, y: 0 } },
+  ])('The world has limits', ({ side, input, commands, expected }) => {
+    test(`on the ${side}:`, () => {
+      const world = World.wrapping(15, 15);
+      const startingPositionRover = initialiseRover(input.x, input.y, input.direction, world);
+      const expectedPositionRover = initialiseRover(expected.x, expected.y, input.direction, world);
+
+      startingPositionRover.move('f');
+
+      expect(startingPositionRover).toEqual(expectedPositionRover);
     });
   });
 });
